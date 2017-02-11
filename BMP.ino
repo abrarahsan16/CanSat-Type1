37113bc8e3cd5b9c654b68faba7409801439e29d
Abrar Ahsan <ahsan.abrar16@hotmail.com>
2017-02-10 21:50:05 -0500

    Test Codes

---
+++ BMP.ino
@@ -0,0 +1,28 @@
+#include <SFE_BMP180.h>
+#include <Wire.h>
+
+SFE_BMP180 bmp;
+double T;
+
+  void setup
+  () {
+  //Setup for BMP Sensor
+  Serial.begin(9600);
+  if (bmp.begin())
+  {
+    Serial.println("I Work");
+  }
+  else
+  {
+    Serial.println("Nope, bye.");
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  bmp.getTemperature(T);
+  Serial.print("Temperature: ");
+  Serial.print(T,2);
+  Serial.print(" deg C");
+  
+}
