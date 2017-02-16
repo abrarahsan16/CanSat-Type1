6e0c887a4a859d56c0e83f76be47668352c104d7
Abrar Ahsan <ahsan.abrar16@hotmail.com>
2017-02-16 09:11:04 -0500

    removed printline

--- Prototype_Cansat/BMP.ino
+++ Prototype_Cansat/BMP.ino
@@ -19,24 +19,24 @@
  void callTemp()
  {
     telemetry[teleTemp] = bmp.getTemperature(T);
-    Serial.print("Temperature: ");
+    /*Serial.print("Temperature: ");
     Serial.print(T,2);
-    Serial.print(" deg C");
+    Serial.print(" deg C");*/
   }
  void callPressure()
  {
     telemetry[telePressure] = bmp.getPressure(P, T);
-    Serial.print("Pressure: ");
+    /*Serial.print("Pressure: ");
     Serial.print(P, 2);
-    Serial.print("mb");
+    Serial.print("mb");*/
     
   }
  void callAlt()
   {
     telemetry[teleAlt] = bmp.altitude(P, P0);
-    Serial.print("Altitude: ");
+    /*Serial.print("Altitude: ");
     Serial.print(A, 2);
     Serial.print("m");
-    Serial.println();
+    Serial.println();*/
   }
 //}
