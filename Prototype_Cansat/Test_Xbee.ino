9e51759aa66513c7ba4d928fe065163b9ec2f254
Abrar Ahsan <ahsan.abrar16@hotmail.com>
2017-02-18 06:42:30 -0500

    Update Test_Xbee.ino

--- Prototype_Cansat/Test_Xbee.ino
+++ Prototype_Cansat/Test_Xbee.ino
@@ -1,31 +1,32 @@
 void radioBegin()
-{ 
+{
   Serial.begin(19200);
+  Serial.println("Hi, I work for once");
 }
 
-void printXB() 
+void printXB()
 {
-  Serial.println("Hi, I work for once");
+
   Serial.print("Mission Time: ");
   Serial.print(telemetry[teleTime]);
   Serial.println("s");
-  
+
   Serial.print("Temperature: ");
   Serial.print(telemetry[teleTemp], 2);
   Serial.println("deg C");
-  
+
   Serial.print("Pressure: ");
   Serial.print(telemetry[telePressure], 2);
   Serial.println("mb");
-  
+
   Serial.print("Altitude: ");
   Serial.print(telemetry[teleAlt], 2);
   Serial.println("m");
-  
-  Serial.print("Heading: ");
-  Serial.print(telemetry[teleHeading],2);
-  Serial.println("degrees from magnetic N");
-  
-  
-}
 
+  Serial.print("Heading: ");
+  Serial.print(telemetry[teleHeading], 2);
+  Serial.println("degrees from magnetic N");
+
+  Serial.print("Count: ");
+  Serial.println(telemetry[teleCount]);
+}
