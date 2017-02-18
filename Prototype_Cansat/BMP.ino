da5f12c1cc0a20f0f1280bc37531b75814a22e19
Abrar Ahsan <ahsan.abrar16@hotmail.com>
2017-02-18 06:41:28 -0500

    Update BMP.ino

--- Prototype_Cansat/BMP.ino
+++ Prototype_Cansat/BMP.ino
@@ -1,13 +1,13 @@
-  double T, P, A, P0;
-  void bmpBegin() 
-  {
-  //Setup for BMP Sensor
-    
-    bmp.begin();
+
+void bmpBegin()
+{
+  //Setup for BMP180
+
+  bmp.begin();
 
   if (bmp.begin())
   {
-    Serial.println("BMP Detected"); 
+    Serial.println("BMP Detected");
   }
   else
   {
@@ -17,27 +17,48 @@
 
 
 //void loop() {
- void callTemp()
- {
-    telemetry[teleTemp] = (float)bmp.getTemperature(T);
-    /*Serial.print("Temperature: ");
-    Serial.print(T,2);
-    Serial.print(" deg C");*/
+void callTemp()
+{
+  status = bmp.startTemperature();
+  if (status != 0)
+  {
+    delay(status);
+    status = bmp.getTemperature(T);
+    if (status != 0)
+    {
+      telemetry[teleTemp] = (float)T;
+      //telemetry[teleTemp] = (float)bae;
+      /*Serial.print("Temperature: ");
+        Serial.print(T,2);
+        Serial.print(" deg C");*/
+    }
   }
- void callPressure()
- {
-    telemetry[telePressure] = (float)bmp.getPressure(P, T);
-    /*Serial.print("Pressure: ");
+}
+void callPressure()
+{
+  status = bmp.startPressure(3);
+  if (status != 0)
+  {
+    delay(status);
+    status = bmp.getPressure(P, T);
+    if (status != 0)
+    {
+      telemetry[telePressure] = (float)P;
+    }
+  }
+  //telemetry[telePressure] = (float)status;
+  /*Serial.print("Pressure: ");
     Serial.print(P, 2);
     Serial.print("mb");*/
-    
-  }
- void callAlt()
-  {
-    telemetry[teleAlt] = (float)bmp.altitude(P, P0);
-    /*Serial.print("Altitude: ");
+
+}
+void callAlt()
+{
+  A=bmp.altitude(P, P0);
+  telemetry[teleAlt] = (float)A;
+  /*Serial.print("Altitude: ");
     Serial.print(A, 2);
     Serial.print("m");
     Serial.println();*/
-  }
+}
 //}
